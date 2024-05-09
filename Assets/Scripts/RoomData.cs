0ac9c362686efb752a0fb107b6a63f020973ec4f
David Pasarin <davidpasarinn@gmail.com>
2024-05-09 11:33:56 +0300

    Initial project + dungeon generator

---
+++ Assets/Scripts/RoomData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomData : MonoBehaviour
+{
+    public Vector3 roomSize = new Vector3(10, 10, 0);
+
+    public int roomDepth = 1;
+}
